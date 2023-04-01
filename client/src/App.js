5e27c23a6374b03d7be592a7bde015bd1d1aa978
Maya Tripathy <maya.tripathy1@gmail.com>
2023-04-01 16:18:29 +0200

    added routes with path for each view

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import { Route, Routes } from "react-router-dom";
 
 import './App.css';
 import NavBar from './components/NavBar';
+import HomeView from './views/HomeView';
+import AboutView from './views/AboutView';
+import ResumeView from './views/ResumeView';
+import ProjectsView from './views/ProjectsView';
+import ContactMeView from './views/ContactMeView';
 
 function App() {
   return (
@@ -16,6 +21,13 @@ function App() {
       <NavBar />
 
       <h1>Maya's Portfolio</h1>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/about" element={<AboutView/>} />
+        <Route path="/resume" element={<ResumeView/>} />
+        <Route path="/projects" element={<ProjectsView/>} />
+        <Route path="/contact-me" element={<ContactMeView/>} />
+      </Routes>
     </div>
   );
 }
