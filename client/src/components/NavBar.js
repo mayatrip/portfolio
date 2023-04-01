0acc6fdde932404530a2c14e98b9c40e921295a1
Maya Tripathy <maya.tripathy1@gmail.com>
2023-04-01 16:22:15 +0200

    added links in the nav bar to reach each view

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 export default function NavBar() {
   return (
     <nav className="NavBar">
-        <a href="#Home">Home</a>
-        <a href="#About">About</a>
-        <a href="#Resume">Resume</a>
-        <a href="#Projects">Projects</a>
-        <a href="#Contact">Contact Me</a>
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <Link to="/about">
+          <p>About</p>
+        </Link>
+        <Link to="/resume">
+          <p>Resume</p>
+        </Link>
+        <Link to="/projects">
+          <p>Projects</p>
+        </Link>
+        <Link to="/contact-me">
+          <p>Contact Me</p>
+        </Link>
     </nav>
   )
 }
